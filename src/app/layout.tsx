4f4b8813acedf20d0c3364afd410b8824a648058
Sohan Rout <sohanrout84@gmail.com>
2026-02-03 18:17:40 +0530

    Feat : added devfolio sdk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Code & Chaos - GDG DCE Hackathon",
   description: "48 hours of innovation, collaboration, and creation. Join 500+ developers building the future at Code & Chaos hackathon.",
   icons: {
-    icon: "/favicon.jpeg",
+    icon: "/favicon.png",
   },
 };
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className="antialiased font-roboto">
         {children}
+        <Script defer src="https://apply.devfolio.co/v2/sdk.js"></Script>
       </body>
     </html>
   );
--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -205,7 +205,12 @@ export default function HeroSection() {
                 className="group font-display px-6 sm:px-8 py-3 sm:py-4 bg-gradient-to-r from-neutral-950 to-neutral-900 text-neutral-199 rounded-xl flex flex-row items-center justify-center gap-2 sm:gap-3 hover:shadow-lg hover:shadow-purple-500/25 transition-all duration-300"
                 strength={0.4}
               >
-                Register Now
+                <div
+  className="apply-button"
+  data-hackathon-slug="code-and-chaos"
+  data-button-theme="dark"
+  style={{ height: "44px", width: "312px" }}
+></div>
               </MagneticButton>
               </div>
               <MagneticButton
