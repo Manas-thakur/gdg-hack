bc44f020e713629e97167527a81dc1a3edccdb7b
Sohan Rout <sohanrout84@gmail.com>
2026-02-19 12:38:28 +0530

    feat : added schedule completion

--- src/sections/ScheduleSection.tsx
+++ src/sections/ScheduleSection.tsx
@@ -19,7 +19,7 @@ const data = [
     time : "9:00 AM",
     content : (
       <div>
-          <h1 className='text-4xl font-display font-semibold'>Hacking Begins</h1>
+          <h1 className='text-4xl font-display font-semibold'>Hacking Starts</h1>
           <p className="mb-8 text-xl text-neutral-400 dark:text-neutral-200 py-2">
             All 50 teams arrive and start building their prototypes.
           </p>
@@ -28,12 +28,24 @@ const data = [
   },
   {
     date : "FEB 20",
-    time : "11:00 AM",
+    time : "11:30 AM",
     content : (
       <div>
-          <h1 className='text-4xl font-display font-semibold'>First Elimination</h1>
+          <h1 className='text-4xl font-display font-semibold'>Judgement Start</h1>
           <p className="mb-8 text-xl text-neutral-400 dark:text-neutral-200 py-2">
             Every team must pitch their "Product Plan + Tech Prototype" to our jury.
+          </p>
+        </div>
+    )
+  },
+  {
+    date : "FEB 20",
+    time : "2:15 PM",
+    content : (
+      <div>
+          <h1 className='text-4xl font-display font-semibold'>Round 1 Results</h1>
+          <p className="mb-8 text-xl text-neutral-400 dark:text-neutral-200 py-2">
+            Declartion of the final Judgement for the first round
           </p>
         </div>
     )
@@ -43,9 +55,117 @@ const data = [
     time : "3:00 PM",
     content : (
       <div>
-          <h1 className='text-4xl font-display font-semibold'>Mentoring Sessions Starts</h1>
+          <h1 className='text-4xl font-display font-semibold'>Team Moves to Round 2</h1>
+          <p className="mb-8 text-xl text-neutral-400 dark:text-neutral-200 py-2">
+            Only the Top 15 Teams (60 participants) who show real potential will be allowed to stay overnight to finish their product.
+          </p>
+        </div>
+    )
+  },
+  {
+    date : "FEB 20",
+    time : "3:00 PM - 4:00 PM",
+    content : (
+      <div>
+          <h1 className='text-4xl font-display font-semibold'>Lunch Break</h1>
+          <p className="mb-8 text-xl text-neutral-400 dark:text-neutral-200 py-2">
+            All 15 teams will be given lunch by the organizers
+          </p>
+        </div>
+    )
+  },
+  {
+    date : "FEB 20",
+    time : "4:00 PM - 6:00 PM",
+    content : (
+      <div>
+          <h1 className='text-4xl font-display font-semibold'>Mentorship Round</h1>
           <p className="mb-8 text-xl text-neutral-400 dark:text-neutral-200 py-2">
             The remaining teams will follow a Milestone-Based schedule.
+          </p>
+        </div>
+    )
+  },
+  {
+    date : "FEB 20",
+    time : "6:00 PM",
+    content : (
+      <div>
+          <h1 className='text-4xl font-display font-semibold'>Hack Period</h1>
+          <p className="mb-8 text-xl text-neutral-400 dark:text-neutral-200 py-2">
+            All 15 teams will continue building their prototypes.
+          </p>
+        </div>
+    )
+  },
+  {
+    date : "FEB 20",
+    time : "8:00 PM - 9:00 PM",
+    content : (
+      <div>
+          <h1 className='text-4xl font-display font-semibold'>Dinner</h1>
+          <p className="mb-8 text-xl text-neutral-400 dark:text-neutral-200 py-2">
+            All 15 teams will be given dinner by the organizers
+          </p>
+        </div>
+    )
+  },
+  {
+    date : "FEB 20",
+    time : "9:00 PM",
+    content : (
+      <div>
+          <h1 className='text-4xl font-display font-semibold'>Hack Period</h1>
+          <p className="mb-8 text-xl text-neutral-400 dark:text-neutral-200 py-2">
+            All 15 teams will continue building their prototypes.
+          </p>
+        </div>
+    )
+  },
+  {
+    date : "FEB 21",
+    time : "12:00 PM - 2:00 PM",
+    content : (
+      <div>
+          <h1 className='text-4xl font-display font-semibold'>Night Mentorship Round</h1>
+          <p className="mb-8 text-xl text-neutral-400 dark:text-neutral-200 py-2">
+            The remaining teams will follow a Milestone-Based schedule.
+          </p>
+        </div>
+    )
+  },
+  {
+    date : "FEB 21",
+    time : "2:00 PM",
+    content : (
+      <div>
+          <h1 className='text-4xl font-display font-semibold'>Hack Period</h1>
+          <p className="mb-8 text-xl text-neutral-400 dark:text-neutral-200 py-2">
+            All 15 teams will continue building their prototypes.
+          </p>
+        </div>
+    )
+  },
+  {
+    date : "FEB 20",
+    time : "7:00 AM - 8:30 AM",
+    content : (
+      <div>
+          <h1 className='text-4xl font-display font-semibold'>Breakfast</h1>
+          <p className="mb-8 text-xl text-neutral-400 dark:text-neutral-200 py-2">
+            All 15 teams will be given breakfast by the organizers
+          </p>
+        </div>
+    )
+  },
+  {
+    date : "FEB 21",
+    time : "8:30 AM",
+    content : (
+      <div>
+          <h1 className='text-4xl font-display font-semibold'>Final Hacking & Submission</h1>
+          <p className="mb-8 text-xl text-neutral-400 dark:text-neutral-200 py-2">
+            Partcipants have to show there final products to our judges.
           </p>
         </div>
     )
@@ -55,7 +175,7 @@ const data = [
     time : "9:00 AM",
     content : (
       <div>
-          <h1 className='text-4xl font-display font-semibold'>JUDGEMENT</h1>
+          <h1 className='text-4xl font-display font-semibold'>Judgement</h1>
           <p className="mb-8 text-xl text-neutral-400 dark:text-neutral-200 py-2">
             Partcipants have to show there final products to our judges.
           </p>
@@ -64,7 +184,19 @@ const data = [
   },
   {
     date : "FEB 21",
-    time : "11:00 AM",
+    time : "12:00 PM",
+    content : (
+      <div>
+          <h1 className='text-4xl font-display font-semibold'>Final Result</h1>
+          <p className="mb-8 text-xl text-neutral-400 dark:text-neutral-200 py-2">
+            With the final judgment all the students will be advised to enter into the closing ceremony.
+          </p>
+        </div>
+    )
+  },
+  {
+    date : "FEB 21",
+    time : "1:00 PM",
     content : (
       <div>
           <h1 className='text-4xl font-display font-semibold'>Closing Ceremony</h1>
